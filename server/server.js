a5c1e3affb3c466972ea3822a7dfeb8a821f7263
Monte DuMond <gdumond42@gmail.com>
2023-01-29 23:08:36 -0500

    comment full CRUD GD

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ app.use(cors({
 require("./config/mongoose.config")
 require("./routes/user.routes")(app)
 require("./routes/post.routes")(app)
+require("./routes/comment.routes")(app)
 
 const server = app.listen(8000,() => console.log('Dojo-Social Running: http://localhost:8000'))
 
